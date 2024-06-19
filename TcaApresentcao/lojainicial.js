429f17d4d317f2ffcfac365bf5058e83edab916a
Everton Silva dos Santos <everton.silva@dbserver.com.br>
2024-06-19 16:57:08 -0300

    sistema

---
+++ TcaApresentcao/lojainicial.js
@@ -0,0 +1,124 @@
+let cart=document.querySelectorAll(".add-cart");
+console.log(cart);
+
+let products=[
+  {
+    name: 'BOX PREMIUM QIX + CHORÃO',
+    price: 999,
+    inCart:0
+
+  },
+
+  {
+    name: 'MOCHILA HOCKS',
+    price: 269,
+    inCart:0
+
+  },
+
+  {
+    name: 'RELÓGIO G-SHOCK',
+    price: 499,
+    inCart:0
+
+  },
+
+  {
+    name: 'BONÉ ALIVE SIDE FIVE',
+    price: 150,
+    inCart:0
+
+  },
+
+  {
+    name: 'GORRO ALIVE PRETO',
+    price: 120,
+    inCart:0
+
+  },
+
+  {
+    name: 'TÊNIS QIX CHORÃO',
+    price: 357,
+    inCart:0
+
+  },
+
+  {
+    name: 'BONÉ ALIVE STUFF STRAPBACK',
+    price: 129,
+    inCart:0
+
+  },
+
+  {
+    name: 'CALÇA ALIVE JEANS BAGGY',
+    price: 309,
+    inCart:0
+
+  },
+
+]
+
+
+for(let i=0;i,cart.length;i++)
+  {
+    cart[i].addEventListener('click', ()=>{
+
+
+      cartNumbers(products[i]);
+      totalCost(products[i]);
+
+    })
+}
+
+
+function cartNumbers(products)
+{
+
+  let productNumbers=localStorage.getItem('cartNumbers');
+  productNumbers=parseInt(productNumbers);
+  if(productNumbers)
+    {
+
+      localStorage.setItem('cartNumbers',productNumbers+1);
+      document.querySelector( '.count-number').textContent=productNumbers+1;
+
+
+    }
+
+    else{
+
+    
+
+  localStorage.setItem('cartNumbers',1);
+
+  // console.log(document.querySelector('.count-number'));
+
+
+  document.querySelector( '.count-number').textContent=1;
+    }
+
+}
+
+
+function totalCost(product)
+{
+
+  let cartCost=localStorage.getItem('totalCost');
+  if(cartCost!=null)
+    {
+
+      cartCost=parseInt(cartCost);
+      localStorage.setItem('totalCost',cartCost+product.price);
+
+    }
+    else{
+
+      localStorage.setItem('totalCost', product.price);
+
+    }
+
+  
+
+}
